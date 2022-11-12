4614f9ba9e13096aff2e5f83b5b9d8ee813e9cb5
VitaliiTushak <ipz224_tvs@student.ztu.edu.ua>
2022-11-12 19:44:22 +0200

    Fifth commit

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Careless Whisper");
 Console.WriteLine("Tonight the music seems so loud");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("I wish that we could lose this crowd");
+Console.WriteLine("Maybe it's better this way");
+Console.WriteLine("We'd hurt each other with the things we'd want to say");
