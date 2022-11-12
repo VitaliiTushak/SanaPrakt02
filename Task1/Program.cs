e682c75574f56720a3813183b727a4a13ffc7036
VitaliiTushak <ipz224_tvs@student.ztu.edu.ua>
2022-11-12 19:43:29 +0200

    Fourth commit

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Careless Whisper");
+Console.WriteLine("Tonight the music seems so loud");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
